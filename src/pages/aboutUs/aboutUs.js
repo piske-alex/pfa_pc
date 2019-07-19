e7c59040876e36ef5e1439ddb644c137256fd46d
alex <alex.wtho@gmail.com>
2019-07-19 16:26:41 +0800

    trade

--- src/pages/aboutUs/aboutUs.js
+++ src/pages/aboutUs/aboutUs.js
@@ -140,6 +140,52 @@ function AboutUs({ history,handleLogout, currentUsername, account }) {
   });
   const [modalOpen, setModalOpen] = React.useState(false);
 
+  window.Clipboard = (function(window, document, navigator) {
+    var textArea,
+      copy;
+
+    function isOS() {
+      return navigator.userAgent.match(/ipad|iphone/i);
+    }
+
+    function createTextArea(text) {
+      textArea = document.createElement('textArea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+    }
+
+    function selectText() {
+      var range,
+        selection;
+
+      if (isOS()) {
+        range = document.createRange();
+        range.selectNodeContents(textArea);
+        selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        textArea.setSelectionRange(0, 999999);
+      } else {
+        textArea.select();
+      }
+    }
+
+    function copyToClipboard() {
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+    }
+
+    copy = function(text) {
+      createTextArea(text);
+      selectText();
+      copyToClipboard();
+    };
+
+    return {
+      copy: copy
+    };
+  })(window, document, navigator);
+
   const handleModalOpen = () => {
     setModalOpen(true);
   };
@@ -196,6 +242,7 @@ function AboutUs({ history,handleLogout, currentUsername, account }) {
   }
 
   const handleCopiedSnackbarOpen =()=> {
+    window.Clipboard.copy(accAddr)
     setCopiedSnackbarOpen(true)
   }
 
@@ -234,6 +281,8 @@ function AboutUs({ history,handleLogout, currentUsername, account }) {
 
             <TextField
               variant={"outlined"}
+              readOnly={false}
+              contentEditable={true}
               value={account.address}
               disabled
             />
