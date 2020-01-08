1875aa395a7db6e1228ae81850e9f241e95e2277
kcps103152joe <103152joee@gmail.com>
2020-01-08 21:53:51 +0800

    news page update

--- src/pages/news/news.js
+++ src/pages/news/news.js
@@ -11,7 +11,7 @@ export default class News extends React.Component {
         }
 
         const CORS = 'https://cors-anywhere.herokuapp.com/';
-        const burl = 'https://www.chainnews.com/zh-hant/feeds/articles/';
+        const burl = 'https://www.chainnews.com/zh-hant/feeds/news/';
         const query = '';
         const params = '';
         const url = burl + query + params;
@@ -19,44 +19,57 @@ export default class News extends React.Component {
         
         feeds.then((res)=>{
                 res.text().then((xml) => {
+                    console.log(xml);
                     var xml_json = new XMLParser().parseFromString(xml);    // Assume xmlText contains the example XML
                     if(xml_json) xml_json = xml_json.children[0].children;
                     console.log(xml_json);
                     this.setState({
                         news: xml_json
                     });
-                    
                 })
             }).catch(() => console.error('Error in fetching the feeds'));
     }
 
+    getParsedDate(strDate){
+        var date = new Date(strDate);
+        var dd = date.getDate();
+        var mm = date.getMonth() + 1; //January is 0!
     
+        var yyyy = date.getFullYear();
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
+        date =  dd + "-" + mm + "-" + yyyy;
+        return date.toString();
+    }
+
     render() {
         const listsStyle = {
             padding: '16px'
         }
         const listStyle = {
             margin: '0 0',
-            padding: '10px 0',
+            padding: '15px 80px 15px 0px',
             borderBottom: '1px solid #555'
         }
         const fontStyle = {
-            fontSize: '1.5em',
+            fontSize: '1.2em',
             color: 'white'
         }
         const dateStyle = {
             fontSize: '0.7em',
-            color: '#555'
+            color: '#555',
+            position: 'absolute',
+            right: '10px'
         }
         const items = this.state.news.map((item, key) =>
             (item.name === 'item') ?
                 <li key={item.children[4].value} style={listStyle}>
-                    <a href={item.children[1].value} target={"_blank"} style={fontStyle}>{item.children[0].value}</a>
-                    <span style={dateStyle}>{item.children[3].value}</span>
+                    <a href={item.children[1].value} target={"_blank"} style={fontStyle}>{item.children[0].value}</a>&nbsp;
+                    <span style={dateStyle}>{this.getParsedDate(item.children[3].value)}</span>
                 </li>
             : ''
         );
-        return(
+        return (
             <div style={listsStyle}>{items}</div>
         )
     }
