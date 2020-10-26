7995df28c74acae4133385fe5a83c8a79eae9a5a
alexho <alex.wtho@gmail.com>
2020-10-26 17:41:26 +0800

    lost files

---
+++ build/precache-manifest.b58f6d36c2ee7404f83f31280cd4bb6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc28075465a828ede8ff34ed1bd393d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8790814c4de8f423a744",
+    "url": "./static/css/2.be67e48b.chunk.css"
+  },
+  {
+    "revision": "802fa1c56576a7d360c8",
+    "url": "./static/css/main.889fe432.chunk.css"
+  },
+  {
+    "revision": "8790814c4de8f423a744",
+    "url": "./static/js/2.389caf8f.chunk.js"
+  },
+  {
+    "revision": "802fa1c56576a7d360c8",
+    "url": "./static/js/main.c4cbb409.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
